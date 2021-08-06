e33ce1256a12502eda43491c0a34b101b4eee9a5
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-08-06 17:40:07 +0000

    Aula 06-08

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Revisão</title>
+    </head>
+    <body>
+        <?php
+        $nome = 'Maria' 
+        ?>
+        <h1>Revisão<h/1>
+        <?php echo $nome; ?>
+    </body>
+</html>
